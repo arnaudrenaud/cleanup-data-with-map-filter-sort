ea17d653f0f2333b06e8a0a654b4cc3d984e9f07
Arnaud Renaud <arnaud.renaud@gmail.com>
2018-09-14 16:35:36 +0200

    Show list of restaurants cleaned up

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const restaurants = require('./restaurants.json');
 
-console.log(restaurants.length);
+const restaurantsCleaned = restaurants
+  .map(restaurant => ({
+    name: restaurant.name,
+    address: restaurant.address1,
+    area: restaurant.area && restaurant.area.name,
+    city: restaurant.city,
+    editorial_rating: restaurant.editorial_rating,
+    description: restaurant.description,
+    annotation: restaurant.annotation,
+    owner_annotation: restaurant.owner_annotation,
+    url: restaurant.to_website,
+    image_url: restaurant.image_url,
+  }))
+console.log(restaurantsCleaned)
